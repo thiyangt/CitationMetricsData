c9f0e32ff3357ceaea8deeb6130b6ccda5fc14f0
thiyangt <tstalagala@gmail.com>
2025-09-25 21:11:26 +0530

    updated

--- data-raw/datacleaning.R
+++ data-raw/datacleaning.R
@@ -1,7 +1,10 @@
 library(tidyverse)
 library(tidyr)
 
-## Table 1:Authors Career
+##2024
+## Table 1:Authors Career: 2024
 library(readxl)
 career.wide.data <- read_excel("data-raw/Table_1_Authors_career_2023_pubs_since_1788_wopp_extracted_202408.xlsx")
 View(career.wide.data)
+
+##2025
